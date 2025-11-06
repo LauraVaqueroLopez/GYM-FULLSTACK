c4d35312507f7b93cfc9b09acc9f82e4deb820a6
Papaseit <alejandropr2005@gmail.com>
2025-11-06 18:22:14 +0100

    fix: error al inicar

--- Backend/models/Cliente.js
+++ Backend/models/Cliente.js
@@ -9,24 +9,27 @@ const Cliente = sequelize.define(
       primaryKey: true,
       defaultValue: DataTypes.UUIDV4,
     },
-    id_usuario: { type: DataTypes.CHAR(36), allowNull: false, unique: true },
+    id_usuario: { 
+      type: DataTypes.CHAR(36), 
+      allowNull: false, 
+      unique: true 
+    },
+
     fecha_nacimiento: DataTypes.DATEONLY,
-<<<<<<< HEAD
     peso: DataTypes.DECIMAL(5, 2),
     altura: DataTypes.DECIMAL(4, 2),
+
     objetivo: DataTypes.ENUM(
       "perder peso",
       "ganar músculo",
       "mejorar resistencia",
       "otro"
     ),
-=======
-  peso: DataTypes.DECIMAL(5, 2),
-  altura: DataTypes.DECIMAL(5, 2),
-    objetivo: DataTypes.ENUM("perder peso","ganar músculo","mejorar resistencia","otro")
->>>>>>> 8c6e238735ad899402d437ff7399cf30678e3e65
   },
-  { tableName: "Clientes", timestamps: false }
+  { 
+    tableName: "Clientes", 
+    timestamps: false 
+  }
 );
 
 export default Cliente;
--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
-<<<<<<< HEAD
 import contratacionRoutes from "./routes/ContratacionRoutes.js";
+import seguimientoRoutes from "./routes/seguimiento.routes.js";
+
 import sequelize from "./config/db_connection.js";
 import "./models/associations.js";
-=======
-import seguimientoRoutes from "./routes/seguimiento.routes.js";
->>>>>>> 8c6e238735ad899402d437ff7399cf30678e3e65
 
 dotenv.config();
 
@@ -18,15 +17,12 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/auth", authRoutes);
-<<<<<<< HEAD
 app.use("/api/contrataciones", contratacionRoutes);
-=======
 app.use("/api/seguimiento", seguimientoRoutes);
->>>>>>> 8c6e238735ad899402d437ff7399cf30678e3e65
 
 const PORT = process.env.PORT || 4000;
 
-// Sincronizar con la BD (opcional)
+// Probar conexión con la BD
 sequelize.authenticate()
   .then(() => console.log("✅ Conectado correctamente a la base de datos MySQL"))
   .catch((err) => console.error("❌ Error al conectar con la base de datos:", err));
